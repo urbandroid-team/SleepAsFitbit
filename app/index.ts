71b3f64065b066b4f83b3ba88140e5c762690a50
Jiří Richter <richtej2@gmail.com>
2019-04-29 15:44:36 +0200

    Comment out fitlogger

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,6 @@
 import { display } from "display";
 import { me } from 'appbit'
 import { Context } from "./controller/context";
-import { memory, launchApp } from "system";
 import { Message } from "./model/message";
 // @ts-ignore
 import fitlogger from "../node_modules/fitbit-logger/app";
@@ -12,11 +11,11 @@ import fitlogger from "../node_modules/fitbit-logger/app";
   var ctx = new Context()
 
 try {
-  fitlogger.init({
-    automaticInterval: 5000,
-    doConsoleLog: true,
-    prefix: 'Logger'
-  })
+  // fitlogger.init({
+  //   automaticInterval: 5000,
+  //   doConsoleLog: true,
+  //   prefix: 'Logger'
+  // })
 
   ctx.ui.initialize()
   ctx.ui.changeToTrackingScreen()
@@ -34,31 +33,7 @@ try {
 
   ctx.msgManager.startCompanionCommChannel()
 
-  // let memLogTimer = setInterval(() => {
-  //   let mem = "M " + memory.js.used + " peak " + memory.js.peak
-  //   ctx.msgManager.msgAdapter.send(new Message("Mem", mem))
-  //   console.log(mem)
-  // }, 20000);
-
-  memory.monitor.onmemorypressurechange = function (a) {
-    let data = memory.monitor.pressure + " " + "M " + memory.js.used + " peak " + memory.js.peak
-    console.log("memoryPressureChange:" + data);
-    ctx.msgManager.msgAdapter.send(new Message("Mem pressure", data))
-  }
-
 } catch (error) {
   console.log(error)
   ctx.msgManager.msgAdapter.send(new Message("error", error))
-}
-
-function measureTicks() {
-  var interval = 100;
-  var last = Date.now();
-  var timer = setInterval(function () {
-    var delta = Date.now() - last - interval;
-    if (delta > 100) {
-      console.error("eventloop_blocked " + delta);
-    }
-    last = Date.now();
-  }, interval);
 }
