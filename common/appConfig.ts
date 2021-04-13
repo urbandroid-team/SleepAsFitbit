462213047ca29ce2158214017558b854c498a002
artaud <richtej2@gmail.com>
2021-04-13 17:19:04 +0200

    debug: turn off debugging
    (cherry picked from commit 2c67a51dc3c9c0d4266c0832830cbc7c4ebc3bde)

--- common/appConfig.ts
+++ common/appConfig.ts
@@ -2,10 +2,10 @@
 export class AppConfig {
   static companionMockSleep = false;
 
-  static DEBUG_COMPANION = true
-  static DEBUG_MAIN = true
-  static DEBUG_SOCKET_MESSAGING_ADAPTER = true
-  static DEBUG_MSG_MANAGER = true
+  static DEBUG_COMPANION = false
+  static DEBUG_MAIN = false
+  static DEBUG_SOCKET_MESSAGING_ADAPTER = false
+  static DEBUG_MSG_MANAGER = false
 
   // functional config
   static defaultCompanionWakeInterval = 5 * 60 * 1000
